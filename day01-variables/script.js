28187f6bd80e97595bdcb0992d22bf4655c93ff8
Andrei Jay <andreijay05@gmail.com>
2025-11-28 22:04:10 +0800

    Finished Day 1 Variables Practice

---
+++ day01-variables/script.js
@@ -0,0 +1,15 @@
+let name = "Andrei Jay";
+let birthYear = 2004;
+let YearNow = 2025;
+
+console.log("Name: " + name);
+console.log("Birth Year: " + birthYear + "\n\n");
+console.log("let's know his birthday! \n\n");
+
+console.log("loading... \n\n");
+
+function Age(birthYear, YearNow){
+    return age = YearNow - birthYear;
+}
+
+console.log(name+"'s current age is " + Age(birthYear, YearNow));
