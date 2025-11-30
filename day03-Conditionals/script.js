ef37c7bb1e8e52debbadfecc0ceb8c7208e35b70
Andrei Jay <andreijay05@gmail.com>
2025-11-30 20:49:31 +0800

    Updated for a better approach for Grade Evaluator

--- day03-Conditionals/script.js
+++ day03-Conditionals/script.js
@@ -1,9 +1,25 @@
-let grade = 90;
+
+let subject1 = 75;
+let subject2 = 87;
+let subject3 = 89;
+
+let grade = Averarge(SumOfGrades(subject1, subject2, subject3), 3);
+
 
 if(grade < 75){
-    console.log("You have failed this subject.");
+    console.log("Your Grade is " + grade.toFixed(2) + "! You have failed this subject.");
 } else if(grade >= 75 && grade <=100){
-    console.log("You have passed this subject");
+    console.log("Your Grade is " + grade.toFixed(2) + "! You have passed this subject");
 } else{
-    console.log("Invalid Grade!");
+    console.log("Your Grade is " + grade.toFixed(2) + "! Invalid Grade!");
+}
+
+
+
+function Averarge(sumOfGrades, numberOfSubject) {
+    return sumOfGrades / numberOfSubject;
+}
+
+function SumOfGrades(sub1, sub2, sub3) {
+    return sub1 + sub2 + sub3;
 }
