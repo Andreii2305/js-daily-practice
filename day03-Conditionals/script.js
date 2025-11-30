25cbc8998fa8b53d9acf68dbbdaf3cb189e5f93e
Andrei Jay <andreijay05@gmail.com>
2025-11-30 20:39:53 +0800

    Simple Grade Evaluator

---
+++ day03-Conditionals/script.js
@@ -0,0 +1,9 @@
+let grade = 90;
+
+if(grade < 75){
+    console.log("You have failed this subject.");
+} else if(grade >= 75 && grade <=100){
+    console.log("You have passed this subject");
+} else{
+    console.log("Invalid Grade!");
+}
